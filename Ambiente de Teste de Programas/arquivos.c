5ec9dce077c96f9f92920636733ae9ab6bb8cd71
Everton <everton.militao@outlook.com.br>
2024-11-28 13:20:22 -0300

    att

---
+++ Ambiente de Teste de Programas/arquivos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// ex1
+void conta_linha(void)
+{
+	FILE* entrada = fopen("entrada.txt", "r");
+
+
+
+	fclose(entrada);
+}
+
+
+
+
+int main(void)
+{
+	
+	
+	
+	
+	return 0;
+}
